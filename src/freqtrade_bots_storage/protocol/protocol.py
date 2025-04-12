5429eb36b388ad1db5e326ca3a5a5a7bf1f794e7
fdoooch <fdoooch@gmail.com>
2025-04-12 19:02:56 -0300

    refactorget_bots

--- src/freqtrade_bots_storage/protocol/protocol.py
+++ src/freqtrade_bots_storage/protocol/protocol.py
@@ -8,30 +8,40 @@ class TradingBotsStorageProtocol(Protocol):
         Returns bot_id
         """
         ...
-    
-    async def get_bot_by_id(self, bot_id: str) -> dict[str, Any]:
-        ...
 
-    async def get_active_bot_by_exchange_and_pair(self, exchange: str, pair: str) -> dict[str, Any] | None:
-        ...
+    async def get_bot_full_data_by_id(self, bot_id: str) -> dict[str, Any]: ...
 
-    async def get_all_bots(self) -> dict[str, dict[str, Any]]:
+
+    async def get_bots(
+        self,
+        exchanges: list[str] | None = None,
+        strategies: list[str] | None = None,
+        statuses: list[str] | None = None,
+        pairs: list[str] | None = None,
+    ) -> dict[str, dict[str, Any]]:
         """
         Returns dict of bots - key is bot_id, value is bot_info
         """
         ...
-        
-    async def delete_bot(self, bot_id: str) -> None:
-        ...
-    
-    async def update_bot_state(self, bot_id: str, update: dict[str, Any]) -> None:
+
+    async def get_bots_with_configs(
+        self,
+        exchanges: list[str] | None = None,
+        strategies: list[str] | None = None,
+        statuses: list[str] | None = None,
+        pairs: list[str] | None = None,
+    ) -> dict[str, dict[str, Any]]:
+        """
+        Returns dict of bots - key is bot_id, value is dict with bot_info and config
+        """
         ...
 
-    async def update_bot_config(self, bot_id: str, update: dict[str, Any]) -> None:
-        ...
+    async def delete_bot(self, bot_id: str) -> None: ...
 
-    async def update_bot_status(self, bot_id: str, status: str) -> None:
-        ...
+    async def update_bot_state(self, bot_id: str, update: dict[str, Any]) -> None: ...
 
-    async def close(self) -> None:
-        ...
+    async def update_bot_config(self, bot_id: str, update: dict[str, Any]) -> None: ...
+
+    async def update_bot_status(self, bot_id: str, status: str) -> None: ...
+
+    async def close(self) -> None: ...
